822278a56d6448a45646e20e09851a38eded423c
DiegoJfsr <diegojfsr@gmail.com>
2024-04-02 10:29:34 -0300

    TiposPrimitivos

--- java_introduction/src/Main.java
+++ java_introduction/src/Main.java
@@ -36,13 +36,24 @@ public class Main {
         // Tipos Primitivos
         // byte, short, int, long
 
+        byte idade = 22; // byte armazena valores pequenos e long valores maiores
+
+        //boolean
+        boolean eBrasileiro = true;
+
+        //char - serve pra armazenar caracteres
+        char letra = 'A';
         
+        //Caso eu queira armazenar uma string completa devo utilizar a Classe String
+        String nome = "Diego"; //String nao é um tipo primitivo, ele é uma classe auxiliar.
+
+        //double, float - numeros de ponto flutuante
+        double idade2 = 22.50;
+        float salario = 400.20F;
 
 
 
-
-
-
+        
 
     }
 }
